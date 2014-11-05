ceafc7250840b4354e4943c4c3ce8d31c195253b
Remy DESTREBECQ <rdestreb@e3r9p16.42.fr>
2014-11-05 16:42:30 +0100

    hdr/libft.h

---
+++ hdr/libft.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdestreb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/03 14:23:08 by rdestreb          #+#    #+#             */
+/*   Updated: 2014/11/05 15:30:14 by rdestreb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+void	*ft_memset(void *s, int n, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putendl(char const *s);
+void	ft_putnbr(int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	*ft_memalloc(size_t size);
+char	*ft_itoa(int n);
+
+size_t	ft_strlen(char const *s);
+char	*ft_strdup(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strcat(char *dest, const char *src);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstlen);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *tosearch, const char *tofind);
+char	*ft_strnstr(const char *tosearch, const char *tofind, size_t n);
+
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+
+#endif
