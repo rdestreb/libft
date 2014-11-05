ef88562aa3f32fffd65a3e780522f7b961921685
Remy DESTREBECQ <rdestreb@e3r9p16.42.fr>
2014-11-05 14:56:36 +0100

    fix strstr = cas particulier

--- src/ft_strstr.c
+++ src/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: rdestreb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 13:41:10 by rdestreb          #+#    #+#             */
-/*   Updated: 2014/11/05 14:04:53 by rdestreb         ###   ########.fr       */
+/*   Updated: 2014/11/05 14:54:05 by rdestreb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strstr(const char *tosearch, const char *tofind)
 	int	j;
 
 	i = -1;
+	if (!(tofind[0]))
+		return ((char*)tosearch);
 	while (tosearch[++i])
 	{
 		if (tosearch[i] == tofind[j])
