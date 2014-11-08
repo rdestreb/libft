09d146ad5f26c2e24dce9d7e6b31b31287f84849
Remy DESTREBECQ <rdestreb@e3r9p15.42.fr>
2014-11-08 13:20:58 +0100

    strmap

--- src/ft_strmap.c
+++ src/ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: rdestreb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 17:29:14 by rdestreb          #+#    #+#             */
-/*   Updated: 2014/11/07 17:37:43 by rdestreb         ###   ########.fr       */
+/*   Updated: 2014/11/08 13:17:32 by rdestreb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 char	*ft_strmap(char const *s, char (*f)(char))
 {
-	
+	char			*new_s;
+	unsigned int	i;
+
+	new_s = ft_strnew(ft_strlen(s));
+	//ft_striter(new_s, (void *)f(*s));
+	i = -1;
+	while (s[++i])
+		new_s[i] = f(s[i]);
+	return (new_s);
 }
